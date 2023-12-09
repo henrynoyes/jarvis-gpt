e09780b901cae5317521c58d62e62e3ef2ded82c
Henry Noyes <noyes.he@northeastern.edu>
2023-12-09 12:24:23 -0500

    stopped at portaudio install

--- featurized_stream.py
+++ featurized_stream.py
@@ -1,7 +1,4 @@
 import speech_recognition as sr
-from gpiozero import Button, LED
-import apa102
-import sounddevice
 from openai import OpenAI
 from elevenlabs import generate, stream
 
@@ -11,23 +8,14 @@ class Jarvis():
         self.client = OpenAI()
 
     def listen(self):
-
-        driver = apa102.APA102(num_led=12)
-        power = LED(5)
-        power.on()
         
         r = sr.Recognizer()
         with sr.Microphone() as source:
             # ambient adjustment causes longer delay
             r.adjust_for_ambient_noise(source)
             print("Listening...")
-        
-            for i in range(12):
-                driver.set_pixel(i, 0, 0, 255)
-            driver.show()
             
             audio = r.listen(source)
-            driver.clear_strip()
 
         try:
             print('Recognizing...')
@@ -40,13 +28,6 @@ class Jarvis():
             return None
         
     def request(self, text):
-        
-        driver = apa102.APA102(num_led=12)
-        for i in range(12):
-            driver.set_pixel(i, 255, 0, 0)
-        driver.show()
-        power = LED(5)
-        power.on()
 
         if text:
             print('Responding...')
@@ -59,7 +40,6 @@ class Jarvis():
             resp_str = response.choices[0].message.content
             
             print(resp_str)
-            driver.clear_strip()
             return resp_str
 
         else:
@@ -67,13 +47,6 @@ class Jarvis():
             return None
 
     def play(self, response):
-        
-        driver = apa102.APA102(num_led=12)
-        for i in range(12):
-            driver.set_pixel(i, 0, 255, 0)
-        driver.show()
-        power = LED(5)
-        power.on()
         
         print('Generating audio...')
         # George, Matthew. Joseph, Daniel
@@ -86,9 +59,6 @@ class Jarvis():
         
         print('Playing audio...')
         stream(audio)
-
-        driver.clear_strip()
-
 
     def run(self):
 
