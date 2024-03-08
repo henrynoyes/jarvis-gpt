146427f9710df912f5a94be4ac3431a6d188abaa
Henry Noyes <hnoyes8@gmail.com>
2024-03-08 11:38:56 -0500

    add state buffer to ignore noise

--- boot_button.py
+++ boot_button.py
@@ -2,12 +2,22 @@ from main import Jarvis, Shutdown
 import RPi.GPIO as GPIO
 from pygame import mixer
 from time import sleep
+from collections import deque
+
+class Buffer(deque):
+    def __init__(self, maxlen=None):
+        super().__init__([0] * maxlen, maxlen=maxlen)
+
+    def zeros(self):
+        self.extend([0] * self.maxlen)
 
 jv = Jarvis()
 
 pin = 13
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+buf = Buffer(maxlen=10)
 
 mixer.init()
 mixer.music.load('./media/welcome_back.mp3')
@@ -21,10 +31,12 @@ try:
 
     while True:
         state = GPIO.input(pin)
+        buf.append(state)
+
+        if all(buf):
+            jv.run()
         
         sleep(0.05)
-        if state:
-            jv.run()
             
 
 except Shutdown:
